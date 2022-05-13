756c51c0f7dafea3adb962129341400749bbe3a9
Iago López <ilopezsmx2011@gmail.com>
2022-05-13 13:09:55 +0200

    First commit

---
+++ templates.py
@@ -0,0 +1,12 @@
+templates = [
+    {
+      "title": "List",
+      "description": "Lorem Ipsum",
+      "url": "++theme++genweb6.theme/tinymce-templates/list.html"
+    },
+    {
+      "title": "Card Group",
+      "description": "Lorem Ipsum",
+      "url": "++theme++genweb6.theme/tinymce-templates/card-group.html"
+    }
+]
